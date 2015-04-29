7d926cec70c93c5766adc5675d920a5a50841635
Mark Allen <maa@xmog.com>
2015-04-29 11:32:21 -0400

    Working on static file deployment processing (in progress)

--- src/main/java/com/soklet/deploy/DeployableArchiveCreator.java
+++ src/main/java/com/soklet/deploy/DeployableArchiveCreator.java
@@ -21,27 +21,38 @@ import static java.util.Arrays.stream;
 import static java.util.Collections.emptyList;
 import static java.util.Collections.emptySet;
 import static java.util.Objects.requireNonNull;
+import static java.util.UUID.randomUUID;
 import static java.util.logging.Level.WARNING;
 import static java.util.stream.Collectors.joining;
 import static java.util.stream.Collectors.toList;
 
+import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.UncheckedIOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.security.MessageDigest;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 import java.util.Set;
+import java.util.SortedSet;
+import java.util.TreeSet;
 import java.util.concurrent.TimeUnit;
+import java.util.function.Function;
 import java.util.logging.Logger;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
+
+import com.soklet.util.PathUtils;
 
 /**
  * @author <a href="http://revetkn.com">Mark Allen</a>
@@ -92,12 +103,60 @@ public abstract class DeployableArchiveCreator {
       staticFileDirectories.stream().forEach(this::verifyValidDirectory);
 
       Set<DeploymentPath> filesToInclude = extractAllFilesFromPaths(pathsToInclude, pathsToExclude);
+      Set<DeploymentPath> staticFilesToInclude = extractStaticFilesFromPaths(staticFileDirectories, filesToInclude);
 
-      // TODO: permit manual filtering
+      // Remove static files for now - we handle them specially below and add them back as we go
+      filesToInclude.removeAll(staticFilesToInclude);
 
-      postProcess();
+      Path temporaryDirectory =
+          Files.createTempDirectory(format("com.soklet.%s-%s-", getClass().getSimpleName(), randomUUID()));
 
-      createZip(archiveName, filesToInclude);
+      try {
+        for (DeploymentPath staticFileToInclude : staticFilesToInclude) {
+          // Make sure we have a place to put it by creating any intermediate directories.
+          // File.mkdirs() actually creates the file so we delete it right after (the directories will stay)
+          File file = temporaryDirectory.resolve(staticFileToInclude.sourcePath()).toFile();
+          file.mkdirs();
+          file.delete();
+
+          Optional<InputStream> transformedStaticFile = transformStaticFile(staticFileToInclude.sourcePath());
+          Path copiedFile = null;
+
+          if (transformedStaticFile.isPresent()) {
+            // TODO: write transformed bytes out as copiedFile
+          } else {
+            copiedFile =
+                Files.copy(staticFileToInclude.sourcePath(),
+                  temporaryDirectory.resolve(staticFileToInclude.sourcePath()));
+          }
+
+          // 1. The static file by itself, e.g. test.css
+          filesToInclude.add(new DeploymentPath(copiedFile, staticFileToInclude.destinationDirectory()));
+
+          // 2. The static file with its hash embedded, e.g. test.BE9748F56259CA49B7E5F249BE1030BE.css
+          String hash = hash(Files.readAllBytes(staticFileToInclude.sourcePath()));
+          String hashedFilename = hashedFilename(staticFileToInclude.sourcePath().toString(), hash);
+          Path copiedHashedFile =
+              Files.copy(staticFileToInclude.sourcePath(), temporaryDirectory.resolve(hashedFilename));
+
+          if (createHashedStaticFiles())
+            filesToInclude.add(new DeploymentPath(copiedHashedFile, staticFileToInclude.destinationDirectory()));
+
+          // 3. The pre-gzipped static file, e.g. test.css.gz
+          // TODO
+
+          // 4. The pre-gzipped hashed static file, e.g. test.BE9748F56259CA49B7E5F249BE1030BE.css.gz
+          // TODO
+        }
+
+        // TODO: permit manual filtering
+
+        postProcess();
+        createZip(archiveName, filesToInclude);
+      } finally {
+        PathUtils.deleteDirectory(temporaryDirectory);
+      }
+
     } finally {
       try {
         performCleanup();
@@ -105,6 +164,39 @@ public abstract class DeployableArchiveCreator {
         logger.log(WARNING, "Unable to perform cleanup", e);
       }
     }
+  }
+
+  protected boolean createHashedStaticFiles() {
+    return true;
+  }
+
+  protected boolean createZippedStaticFiles() {
+    return true;
+  }
+
+  protected Optional<InputStream> transformStaticFile(Path staticFile) {
+    return Optional.empty();
+  }
+
+  protected Set<DeploymentPath> extractStaticFilesFromPaths(Set<Path> staticFileDirectories,
+      Set<DeploymentPath> filesToInclude) {
+    requireNonNull(staticFileDirectories);
+    requireNonNull(filesToInclude);
+
+    Set<DeploymentPath> staticPathsToInclude = new HashSet<>();
+
+    for (Path staticFileDirectory : staticFileDirectories) {
+      Path parentDirectory = staticFileDirectory.toAbsolutePath();
+
+      for (DeploymentPath fileToInclude : filesToInclude) {
+        Path possibleChildFile = fileToInclude.sourcePath().toAbsolutePath();
+
+        if (possibleChildFile.startsWith(parentDirectory))
+          staticPathsToInclude.add(fileToInclude);
+      }
+    }
+
+    return staticPathsToInclude;
   }
 
   protected void createZip(String archiveName, Set<DeploymentPath> filesToInclude) {
@@ -120,14 +212,25 @@ public abstract class DeployableArchiveCreator {
     }
 
     ZipOutputStream zipOutputStream = null;
+    Function<DeploymentPath, String> zipEntryNameProvider =
+        (deploymentPath) -> format("%s/%s", deploymentPath.destinationDirectory(), deploymentPath.sourcePath()
+          .getFileName());
 
     try {
       zipOutputStream = new ZipOutputStream(fileOutputStream);
       zipOutputStream.setLevel(9);
 
-      for (DeploymentPath deploymentPath : filesToInclude) {
-        String zipEntryName =
-            format("%s/%s", deploymentPath.destinationDirectory(), deploymentPath.sourcePath().getFileName());
+      SortedSet<DeploymentPath> sortedFilesToInclude = new TreeSet<DeploymentPath>(new Comparator<DeploymentPath>() {
+        @Override
+        public int compare(DeploymentPath deploymentPath1, DeploymentPath deploymentPath2) {
+          return zipEntryNameProvider.apply(deploymentPath1).compareTo(zipEntryNameProvider.apply(deploymentPath2));
+        }
+      });
+
+      sortedFilesToInclude.addAll(filesToInclude);
+
+      for (DeploymentPath deploymentPath : sortedFilesToInclude) {
+        String zipEntryName = zipEntryNameProvider.apply(deploymentPath);
         logger.info(format("Adding %s...", zipEntryName));
         zipOutputStream.putNextEntry(new ZipEntry(zipEntryName));
         zipOutputStream.write(Files.readAllBytes(deploymentPath.sourcePath()));
@@ -278,5 +381,32 @@ public abstract class DeployableArchiveCreator {
   protected boolean invalidProcessExitValue(Process process) {
     requireNonNull(process);
     return process.exitValue() != 0;
+  }
+
+  protected String hashedFilename(String filename, String hash) {
+    requireNonNull(filename);
+    requireNonNull(hash);
+
+    int lastIndexOfPeriod = filename.lastIndexOf(".");
+
+    if (lastIndexOfPeriod == -1)
+      return format("%s.%s", filename, hash);
+
+    return format("%s.%s%s", filename.substring(0, lastIndexOfPeriod), hash, filename.substring(lastIndexOfPeriod));
+  }
+
+  protected String hash(byte[] bytes) throws Exception {
+    requireNonNull(bytes);
+
+    MessageDigest messageDigest = MessageDigest.getInstance("MD5");
+    byte[] hashBytes = messageDigest.digest(bytes);
+    StringBuilder stringBuilder = new StringBuilder(2 * hashBytes.length);
+
+    for (byte b : hashBytes) {
+      stringBuilder.append("0123456789ABCDEF".charAt((b & 0xF0) >> 4));
+      stringBuilder.append("0123456789ABCDEF".charAt((b & 0x0F)));
+    }
+
+    return stringBuilder.toString();
   }
 }
