0ba600bfda089cfbd6a1b36f1137ccc520e34bdc
Mark Allen <mark.a.allen@gmail.com>
2024-09-22 18:00:40 -0400

    Javadoc for `ResourceMethodResolver`

--- src/main/java/com/soklet/core/ResourceMethodResolver.java
+++ src/main/java/com/soklet/core/ResourceMethodResolver.java
@@ -21,12 +21,31 @@ import java.util.Optional;
 import java.util.Set;
 
 /**
+ * Contract for matching incoming HTTP requests with appropriate <em>Resource Methods</em> (Java methods to invoke to handle requests).
+ * <p>
+ * Soklet's default implementation of this type, {@link com.soklet.core.impl.DefaultResourceMethodResolver}, is sufficient for most applications.
+ * <p>
+ * However, should a custom implementation be necessary for your application, documentation is available at <a href="https://www.soklet.com/docs/request-handling#resource-method-resolution">https://www.soklet.com/docs/request-handling#resource-method-resolution</a>.
+ *
  * @author <a href="https://www.revetkn.com">Mark Allen</a>
  */
 public interface ResourceMethodResolver {
+	/**
+	 * Given an HTTP request, provide a matching <em>Resource Method</em> to invoke.
+	 * <p>
+	 * An unmatched <em>Resource Method</em> generally indicates an {@code HTTP 404}.
+	 *
+	 * @param request the HTTP request
+	 * @return the matching <em>Resource Method</em>, or {@link Optional#empty()} if no match was found
+	 */
 	@Nonnull
 	Optional<ResourceMethod> resourceMethodForRequest(@Nonnull Request request);
 
+	/**
+	 * Vends the set of all <em>Resource Methods</em> registered in the system.
+	 *
+	 * @return the set of all <em>Resource Methods</em> in the system
+	 */
 	@Nonnull
 	Set<ResourceMethod> getResourceMethods();
 }
