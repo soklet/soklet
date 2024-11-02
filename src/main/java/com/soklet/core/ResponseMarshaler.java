2b12143e3e81db8b41db7d1c7539651e31441ed4
Mark Allen <mark.a.allen@gmail.com>
2024-11-02 11:32:12 -0400

    Javadoc for `ResponseMarshaler`

--- src/main/java/com/soklet/core/ResponseMarshaler.java
+++ src/main/java/com/soklet/core/ResponseMarshaler.java
@@ -25,17 +25,21 @@ import java.util.Set;
  * <p>
  * The {@link MarshaledResponse} value returned from these methods is what is ultimately sent back to
  * clients as bytes over the wire.
+ * <p>
+ * Full documentation is available at <a href="https://www.soklet.com/docs/response-writing">https://www.soklet.com/docs/response-writing</a>.
  *
  * @author <a href="https://www.revetkn.com">Mark Allen</a>
  */
 public interface ResponseMarshaler {
 	/**
-	 * Writes a "happy path" response - the request was matched to a resource method and executed non-exceptionally.
+	 * Prepares a "happy path" response - the request was matched to a resource method and executed non-exceptionally.
+	 * <p>
+	 * Detailed documentation is available at <a href="https://www.soklet.com/docs/response-writing#happy-path">https://www.soklet.com/docs/response-writing#happy-path</a>.
 	 *
 	 * @param request        the HTTP request
 	 * @param response       the response provided by the resource method that handled the request
 	 * @param resourceMethod the resource method that handled the request
-	 * @return the response to send over the wire
+	 * @return the response to be sent over the wire
 	 */
 	@Nonnull
 	MarshaledResponse forHappyPath(@Nonnull Request request,
@@ -43,79 +47,120 @@ public interface ResponseMarshaler {
 																 @Nonnull ResourceMethod resourceMethod);
 
 	/**
-	 * Writes a response for a request that triggers an
+	 * Prepares a response for a request that triggers an
 	 * <a href="https://httpwg.org/specs/rfc9110.html#status.404">HTTP 404 Not Found</a>.
+	 * <p>
+	 * Detailed documentation is available at <a href="https://www.soklet.com/docs/response-writing#404-not-found">https://www.soklet.com/docs/response-writing#404-not-found</a>.
 	 *
 	 * @param request the HTTP request
-	 * @return the response to send over the wire
+	 * @return the response to be sent over the wire
 	 */
 	@Nonnull
 	MarshaledResponse forNotFound(@Nonnull Request request);
 
 	/**
-	 * Writes a response for a request that triggers an
+	 * Prepares a response for a request that triggers an
 	 * <a href="https://httpwg.org/specs/rfc9110.html#status.405">HTTP 405 Method Not Allowed</a>.
+	 * <p>
+	 * Detailed documentation is available at <a href="https://www.soklet.com/docs/response-writing#405-method-not-allowed">https://www.soklet.com/docs/response-writing#405-method-not-allowed</a>.
 	 *
 	 * @param request            the HTTP request
 	 * @param allowedHttpMethods appropriate HTTP methods to write to the {@code Allow} response header
-	 * @return the response to send over the wire
+	 * @return the response to be sent over the wire
 	 */
 	@Nonnull
 	MarshaledResponse forMethodNotAllowed(@Nonnull Request request,
 																				@Nonnull Set<HttpMethod> allowedHttpMethods);
 
 	/**
-	 * Writes a response for a request that triggers an
-	 * <a href="https://httpwg.org/specs/rfc9110.html#status.413">HTTP 413 Content Too Large</a>.
+	 * Prepares a response for a request that triggers an <a href="https://httpwg.org/specs/rfc9110.html#status.413">HTTP 413 Content Too Large</a>.
+	 * <p>
+	 * Detailed documentation is available at <a href="https://www.soklet.com/docs/response-writing#413-content-too-large">https://www.soklet.com/docs/response-writing#413-content-too-large</a>.
 	 *
 	 * @param request        the HTTP request
 	 * @param resourceMethod the resource method that would have handled the request, if available
-	 * @return the response to send over the wire
+	 * @return the response to be sent over the wire
 	 */
 	@Nonnull
 	MarshaledResponse forContentTooLarge(@Nonnull Request request,
 																			 @Nullable ResourceMethod resourceMethod);
 
 	/**
-	 * Writes a response for an HTTP {@code OPTIONS} request.
+	 * Prepares a response for an HTTP {@code OPTIONS} request.
 	 * <p>
 	 * Note that CORS preflight responses are handled specially by {@link #forCorsPreflightAllowed(Request, CorsPreflight, CorsPreflightResponse)}
 	 * and {@link #forCorsPreflightRejected(Request, CorsPreflight)} - not this method.
+	 * <p>
+	 * Detailed documentation is available at <a href="https://www.soklet.com/docs/response-writing#http-options">https://www.soklet.com/docs/response-writing#http-options</a>.
 	 *
 	 * @param request            the HTTP request
 	 * @param allowedHttpMethods appropriate HTTP methods to write to the {@code Allow} response header
-	 * @return the response to send over the wire
+	 * @return the response to be sent over the wire
 	 */
 	@Nonnull
 	MarshaledResponse forOptions(@Nonnull Request request,
 															 @Nonnull Set<HttpMethod> allowedHttpMethods);
 
+	/**
+	 * Prepares a response for scenarios in which an uncaught exception is encountered.
+	 * <p>
+	 * Detailed documentation is available at <a href="https://www.soklet.com/docs/response-writing#uncaught-exceptions">https://www.soklet.com/docs/response-writing#uncaught-exceptions</a>.
+	 *
+	 * @param request        the HTTP request
+	 * @param throwable      the exception that was thrown
+	 * @param resourceMethod the resource method that would have handled the request, if available
+	 * @return the response to be sent over the wire
+	 */
 	@Nonnull
 	MarshaledResponse forThrowable(@Nonnull Request request,
 																 @Nonnull Throwable throwable,
 																 @Nullable ResourceMethod resourceMethod);
 
+	/**
+	 * Prepares a response for an HTTP {@code HEAD} request.
+	 * <p>
+	 * Detailed documentation is available at <a href="https://www.soklet.com/docs/response-writing#http-head">https://www.soklet.com/docs/response-writing#http-head</a>.
+	 *
+	 * @param request                    the HTTP request
+	 * @param getMethodMarshaledResponse the binary data that would have been sent over the wire for an equivalent {@code GET} request (necessary in order to write the {@code Content-Length} header for a {@code HEAD} response)
+	 * @return the response to be sent over the wire
+	 */
 	@Nonnull
 	MarshaledResponse forHead(@Nonnull Request request,
 														@Nonnull MarshaledResponse getMethodMarshaledResponse);
 
+	/**
+	 * Prepares a response for "CORS preflight allowed" scenario when your {@link CorsAuthorizer} approves a preflight request.
+	 * <p>
+	 * Detailed documentation is available at <a href="https://www.soklet.com/docs/cors#writing-cors-responses">https://www.soklet.com/docs/cors#writing-cors-responses</a>.
+	 *
+	 * @param request               the HTTP request
+	 * @param corsPreflight         the CORS preflight request data
+	 * @param corsPreflightResponse the data that should be included in this CORS preflight response
+	 * @return the response to be sent over the wire
+	 */
 	@Nonnull
 	MarshaledResponse forCorsPreflightAllowed(@Nonnull Request request,
 																						@Nonnull CorsPreflight corsPreflight,
 																						@Nonnull CorsPreflightResponse corsPreflightResponse);
 
 	/**
-	 * @param request
-	 * @return the response to send over the wire
+	 * Prepares a response for "CORS preflight rejected" scenario when your {@link CorsAuthorizer} denies a preflight request.
+	 * <p>
+	 * Detailed documentation is available at <a href="https://www.soklet.com/docs/cors#writing-cors-responses">https://www.soklet.com/docs/cors#writing-cors-responses</a>.
+	 *
+	 * @param request       the HTTP request
+	 * @param corsPreflight the CORS preflight request data
+	 * @return the response to be sent over the wire
 	 */
 	@Nonnull
 	MarshaledResponse forCorsPreflightRejected(@Nonnull Request request,
 																						 @Nonnull CorsPreflight corsPreflight);
 
 	/**
-	 * Applies "CORS is permitted for this request" data to the response.
+	 * Applies "CORS is permitted for this request" data to a response.
 	 * <p>
-	 * Invoked for any non-preflight CORS request that {@link CorsAuthorizer} approves.
+	 * Invoked for any non-preflight CORS request that your {@link CorsAuthorizer} approves.
 	 * <p>
 	 * This method will normally return a copy of the {@code marshaledResponse} with these headers applied
 	 * based on the values of {@code corsResponse}:
@@ -129,7 +174,7 @@ public interface ResponseMarshaler {
 	 * @param cors              the CORS request data
 	 * @param corsResponse      CORS response data to write as specified by {@link CorsAuthorizer}
 	 * @param marshaledResponse the existing response to which we should apply relevant CORS headers
-	 * @return the response to send over the wire
+	 * @return the response to be sent over the wire
 	 */
 	@Nonnull
 	MarshaledResponse forCorsAllowed(@Nonnull Request request,
