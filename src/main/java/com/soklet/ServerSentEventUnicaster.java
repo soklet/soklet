3bcf9dd7ac234dea4580f0a6d93259cea00035f9
Mark Allen <mark.a.allen@gmail.com>
2025-10-14 18:02:16 -0400

    Exploring the idea of `ServerSentEventUnicaster`

---
+++ src/main/java/com/soklet/ServerSentEventUnicaster.java
@@ -0,0 +1,62 @@
+/*
+ * Copyright 2022-2025 Revetware LLC.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.soklet;
+
+import javax.annotation.Nonnull;
+import java.util.List;
+
+import static java.util.Objects.requireNonNull;
+
+/**
+ * Unicasts a <a href="https://developer.mozilla.org/en-US/docs/Web/API/Server-sent_events/Using_server-sent_events">Server-Sent Event</a> payload to a specific client listening on a {@link ResourcePath}.
+ * <p>
+ * For example:
+ * <pre>{@code TODO}</pre>
+ * <p>
+ * See <a href="https://www.soklet.com/docs/server-sent-events">https://www.soklet.com/docs/server-sent-events</a> for detailed documentation.
+ * <p>
+ * Formal specification is available at <a href="https://html.spec.whatwg.org/multipage/server-sent-events.html#server-sent-events">https://html.spec.whatwg.org/multipage/server-sent-events.html#server-sent-events</a>.
+ *
+ * @author <a href="https://www.revetkn.com">Mark Allen</a>
+ */
+@FunctionalInterface
+public interface ServerSentEventUnicaster {
+	/**
+	 * Unicasts a single Server-Sent Event payload to a specific client listening to this broadcaster's {@link ResourcePath}.
+	 * <p>
+	 * In practice, implementations will generally return "immediately" and unicast operation[s] will occur on separate threads of execution.
+	 * <p>
+	 * However, mock implementations may wish to block until the unicast has completed - for example, to simplify automated testing.
+	 *
+	 * @param serverSentEvent the Server-Sent Event payload to unicast
+	 */
+	default void unicast(@Nonnull ServerSentEvent serverSentEvent) {
+		requireNonNull(serverSentEvent);
+		unicast(List.of(serverSentEvent));
+	}
+
+	/**
+	 * Unicasts a list of Server-Sent Event payload to a specific client listening to this broadcaster's {@link ResourcePath}.
+	 * <p>
+	 * In practice, implementations will generally return "immediately" and unicast operation[s] will occur on separate threads of execution.
+	 * <p>
+	 * However, mock implementations may wish to block until the unicasts have completed - for example, to simplify automated testing.
+	 *
+	 * @param serverSentEvents the Server-Sent Event payloads to unicast
+	 */
+	void unicast(@Nonnull List<ServerSentEvent> serverSentEvents);
+}
