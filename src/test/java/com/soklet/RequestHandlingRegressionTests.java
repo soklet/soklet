8b65aeba42b35384cc6e6776424b351568033fc6
Mark Allen <mark.a.allen@gmail.com>
2025-12-29 10:49:21 -0500

    Tests for previous commits

---
+++ src/test/java/com/soklet/RequestHandlingRegressionTests.java
@@ -0,0 +1,154 @@
+/*
+ * Copyright 2022-2026 Revetware LLC.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.soklet;
+
+import com.soklet.annotation.GET;
+import org.jspecify.annotations.NonNull;
+import org.jspecify.annotations.Nullable;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import javax.annotation.concurrent.ThreadSafe;
+import java.util.Map;
+import java.util.Set;
+import java.util.function.Consumer;
+import java.util.function.Function;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+/**
+ * @author <a href="https://www.revetkn.com">Mark Allen</a>
+ */
+@ThreadSafe
+public class RequestHandlingRegressionTests {
+	private static final String ORIGIN = "https://example.com";
+
+	@Test
+	public void wrappedRequestIsUsedForInterceptAndResponseLogic() {
+		SokletConfig configuration = SokletConfig.forSimulatorTesting()
+				.resourceMethodResolver(ResourceMethodResolver.withClasses(Set.of(WrappedRequestResource.class)))
+				.corsAuthorizer(CorsAuthorizer.withAcceptAllPolicy())
+				.requestInterceptor(new RequestInterceptor() {
+					@Override
+					public void wrapRequest(@NonNull Request request,
+																	@Nullable ResourceMethod resourceMethod,
+																	@NonNull Consumer<Request> requestConsumer) {
+						Request wrappedRequest = request.copy()
+								.httpMethod(HttpMethod.HEAD)
+								.headers(headers -> {
+									headers.put("Origin", Set.of(ORIGIN));
+									headers.put("X-Wrapped", Set.of("true"));
+								})
+								.finish();
+
+						requestConsumer.accept(wrappedRequest);
+					}
+
+					@Override
+					public void interceptRequest(@NonNull Request request,
+																			 @Nullable ResourceMethod resourceMethod,
+																			 @NonNull Function<Request, MarshaledResponse> requestHandler,
+																			 @NonNull Consumer<MarshaledResponse> marshaledResponseConsumer) {
+						marshaledResponseConsumer.accept(requestHandler.apply(request));
+					}
+				})
+				.lifecycleObserver(new LifecycleObserver() {
+					@Override
+					public void didReceiveLogEvent(@NonNull LogEvent logEvent) { /* quiet */ }
+				})
+				.build();
+
+		Soklet.runSimulator(configuration, simulator -> {
+			RequestResult result = simulator.performRequest(
+					Request.withPath(HttpMethod.GET, "/greet").build());
+
+			MarshaledResponse response = result.getMarshaledResponse();
+			assertEquals(Integer.valueOf(200), response.getStatusCode());
+			Assertions.assertTrue(response.getBody().isEmpty(), "HEAD response should not include a body");
+			assertEquals(Set.of("5"), response.getHeaders().get("Content-Length"));
+			assertEquals(Set.of(ORIGIN), response.getHeaders().get("Access-Control-Allow-Origin"));
+		});
+	}
+
+	@Test
+	public void responseMarshalerRespectsCaseInsensitiveContentType() {
+		SokletConfig configuration = SokletConfig.forSimulatorTesting()
+				.resourceMethodResolver(ResourceMethodResolver.withClasses(Set.of(ContentTypeResource.class)))
+				.lifecycleObserver(new LifecycleObserver() {
+					@Override
+					public void didReceiveLogEvent(@NonNull LogEvent logEvent) { /* quiet */ }
+				})
+				.build();
+
+		Soklet.runSimulator(configuration, simulator -> {
+			RequestResult result = simulator.performRequest(
+					Request.withPath(HttpMethod.GET, "/content-type").build());
+
+			assertEquals(Integer.valueOf(200), result.getMarshaledResponse().getStatusCode());
+			assertEquals(Set.of("application/custom"),
+					result.getMarshaledResponse().getHeaders().get("Content-Type"));
+		});
+	}
+
+	@Test
+	public void multipleRouteDeclarationsDoNotAffectSpecificity() {
+		ResourceMethodResolver resolver = ResourceMethodResolver.withClasses(
+				Set.of(MultiGetResource.class, ItemsResource.class));
+
+		Request request = Request.withPath(HttpMethod.GET, "/items/123").build();
+
+		assertThrows(RuntimeException.class,
+				() -> resolver.resourceMethodForRequest(request, ServerType.STANDARD_HTTP));
+	}
+
+	@ThreadSafe
+	public static class WrappedRequestResource {
+		@GET("/greet")
+		public String greet() {
+			return "hello";
+		}
+	}
+
+	@ThreadSafe
+	public static class ContentTypeResource {
+		@GET("/content-type")
+		public Response contentType() {
+			return Response.withStatusCode(200)
+					.headers(Map.of("content-type", Set.of("application/custom")))
+					.body("ok")
+					.build();
+		}
+	}
+
+	@ThreadSafe
+	public static class MultiGetResource {
+		@GET("/items/{id}")
+		@GET("/items/special")
+		public String getItem() {
+			return "ok";
+		}
+	}
+
+	@ThreadSafe
+	public static class ItemsResource {
+		@GET("/items/{id}")
+		public String getItem() {
+			return "ok";
+		}
+	}
+}
