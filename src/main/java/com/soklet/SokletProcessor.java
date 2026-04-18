d2d3bebe3e1f92b904d1f375e0496876e0285ffb
Mark Allen <mark.a.allen@gmail.com>
2026-04-18 12:09:28 -0400

    3.0.1 release

--- src/main/java/com/soklet/SokletProcessor.java
+++ src/main/java/com/soklet/SokletProcessor.java
@@ -107,7 +107,7 @@ import java.util.stream.Collectors;
  *     </configuration>
  * </plugin>}</pre>
  * Using <a href="https://gradle.org" target="_blank">Gradle</a>:
- * <pre>{@code def sokletVersion = "3.0.0" // (use your actual version)
+ * <pre>{@code def sokletVersion = "3.0.1" // (use your actual version)
  *
  * dependencies {
  *   // Soklet used by your code at compile/run time
